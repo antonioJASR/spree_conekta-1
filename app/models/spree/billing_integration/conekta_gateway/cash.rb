62fcb7f4e793297b26a028c41f22d424cd611a2a
José Antonio Sánchez Reynaga <joanzare@gmail.com>
2019-06-07 23:35:27 -0500

    Update cash.rb

--- app/models/spree/billing_integration/conekta_gateway/cash.rb
+++ app/models/spree/billing_integration/conekta_gateway/cash.rb
@@ -3,10 +3,6 @@ module Spree
     preference :auth_token, :string
     preference :source_method, :string, default: 'oxxo'
 
-    unless Rails::VERSION::MAJOR == 4
-      attr_accessible :preferred_auth_token, :preferred_source_method, :gateway_response
-    end
-
     def provider_class
       Spree::Conekta::Provider
     end
